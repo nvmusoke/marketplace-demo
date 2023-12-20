1f4fb4710baeead80969a52a9b45974f5b86b2f8
Namanda Musoke <nmusoke@vertify.com>
2023-12-19 18:15:09 -0600

    rm usestate

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -3,7 +3,6 @@
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 import { useSearchParams, useRouter } from 'next/navigation';
-import { useState } from 'react';
 import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
