dc58a84f77592e90e23be326c2e93dbe6411bcdb
Namanda Musoke <nmusoke@vertify.com>
2023-12-31 22:07:44 -0600

    fix errors

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -6,6 +6,8 @@ import { auth } from '@/auth';
 export default async function Component() {
   const { user } = (await auth()) ?? {};
 
+  const isLoggedIn = user !== undefined;
+
   return (
     <div className="flex min-h-screen flex-col">
       <main className="flex-1 gap-8 p-8">
@@ -13,7 +15,7 @@ export default async function Component() {
           {`Welcome Back, ${user?.name}!`}
         </Typography>
         <div className="grid grid-cols-1 gap-4 pt-36 md:grid-cols-2 lg:grid-cols-3">
-          <TitleCards />
+          <TitleCards isLoggedIn={isLoggedIn} />
         </div>
       </main>
       <footer className="flex items-center justify-end space-x-4 p-6">
--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Component() {
     <div className="flex min-h-screen flex-col">
       <main className="flex-1 gap-8 p-6">
         <div className="grid grid-cols-1 gap-4 pt-40 md:grid-cols-2 lg:grid-cols-3">
-          <TitleCards />
+          <TitleCards isLoggedIn={false} />
         </div>
       </main>
       <footer className="flex items-center justify-end space-x-4 p-6">
